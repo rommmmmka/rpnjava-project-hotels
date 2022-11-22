cf13d043a7cd175c2e11426541f12370fab5a171
Raman Kravets <rommmmmka.m@gmail.com>
2022-11-22 07:21:29 +0300

    hid order button when user not logged in, unified rooms name

--- main/java/com/kravets/hotels/rpnjava/exception/NoFreeRoomsAvaliableException.java
+++ main/java/com/kravets/hotels/rpnjava/exception/NoFreeRoomsAvaliableException.java
@@ -2,6 +2,6 @@ package com.kravets.hotels.rpnjava.exception;
 
 public class NoFreeRoomsAvaliableException extends Exception{
     public NoFreeRoomsAvaliableException() {
-        super("Вольных пакояў больш не засталося");
+        super("Вольных нумароў больш не засталося");
     }
 }
